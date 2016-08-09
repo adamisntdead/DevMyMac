5ce0f23002ea86192a7e7354555cbee10cf96c42
Adam Kelly <adam.guitar12@gmail.com>
2016-08-09 12:05:31 +0100

    Add Python, Java, Unity, and Android Developmetn Setup

--- setup.sh
+++ setup.sh
@@ -1,81 +1,202 @@
+# _______  ___      _______  _______  __    _
+# |       ||   |    |       ||   _   ||  |  | |
+# |       ||   |    |    ___||  |_|  ||   |_| |
+# |       ||   |    |   |___ |       ||       |
+# |      _||   |___ |    ___||       ||  _    |
+# |     |_ |       ||   |___ |   _   || | |   |
+# |_______||_______||_______||__| |__||_|  |__|
+# __   __  _______  _______
+# |  |_|  ||   _   ||       |
+# |       ||  |_|  ||       |
+# |       ||       ||       |
+# |       ||       ||      _|
+# | ||_|| ||   _   ||     |_
+# |_|   |_||__| |__||_______|
+# _______  _______  _______  __   __  _______
+# |       ||       ||       ||  | |  ||       |
+# |  _____||    ___||_     _||  | |  ||    _  |
+# | |_____ |   |___   |   |  |  |_|  ||   |_| |
+# |_____  ||    ___|  |   |  |       ||    ___|
+# _____| ||   |___   |   |  |       ||   |
+# |_______||_______|  |___|  |_______||___|    
+
+# Color Variables
+RED='\033[0;31m'
+GREEN='\033[0;32m'
+NC='\033[0m' # No Color
+
+# Ask for the administrator password upfront.
+sudo -v
+
+# Keep Sudo Until Script is finished
+while true; do sudo -n true; sleep 60; kill -0 "$$" || exit; done 2>/dev/null &
+
 # Check if OSX Command line tools are installed
 if type xcode-select >&- && xpath=$( xcode-select --print-path ) &&
     test -d "${xpath}" && test -x "${xpath}" ; then
     ###############################################################################
     # Computer Settings                                                           #
     ###############################################################################
-    echo "Enter your computer name please?"
+    echo -e "${RED}Enter your computer name please?${NC}"
     read cpname
-    
+
     sudo scutil --set ComputerName "$cpname"
     sudo scutil --set HostName "$cpname"
     sudo scutil --set LocalHostName "$cpname"
     defaults write /Library/Preferences/SystemConfiguration/com.apple.smb.server NetBIOSName -string "$cpname"
-    
-    echo "Enable repeat on keydown"
+
     defaults write -g ApplePressAndHoldEnabled -bool false
-    
-    echo "Show Path bar in Finder"
     defaults write com.apple.finder ShowPathbar -bool true
-    
-    echo "Show Status bar in Finder"
     defaults write com.apple.finder ShowStatusBar -bool true
-    
-    echo "Set a blazingly fast keyboard repeat rate"
     defaults write NSGlobalDomain KeyRepeat -int 0.02
-    
-    echo "Set a shorter Delay until key repeat"
     defaults write NSGlobalDomain InitialKeyRepeat -int 12
-    
-    echo "Show the ~/Library folder"
     chflags nohidden ~/Library
-    
+
     echo "set git config values"
-    echo "Please enter your name?"
+    echo -e "${RED}Please enter your name?${NC}"
     read name
     git config --global user.name "$name"
-    echo "Please enter your git email?"
+    echo -e "${RED}Please enter your git email?${NC}"
     read email
     git config --global user.email "$email"
-    
+
     git config --global color.ui true
-    
+
     ###############################################################################
     # Install Applications                                                        #
     ###############################################################################
-    
-    # Install OhMyZsh
-    echo "Installing OhMyZsh"
+
     sh -c "$(curl -fsSL https://raw.githubusercontent.com/robbyrussell/oh-my-zsh/master/tools/install.sh)"
-    
+
     # Install Homebrew
     echo "Installing Homebrew"
     /usr/bin/ruby -e "$(curl -fsSL https://raw.githubusercontent.com/Homebrew/install/master/install)"
-    
-    # Install Nodejs
-    echo "Installing NVM"
-    curl -o- https://raw.githubusercontent.com/creationix/nvm/v0.31.4/install.sh | bash
-    
-    echo "Installing Nodejs"
-    nvm install node
-    nvm use node
-    
+
+    echo -e "${RED}Install NodeJS? ${NC}[y/N]"
+    read -n 1 -r
+    echo    # (optional) move to a new line
+    if [[ $REPLY =~ ^[Yy]$ ]]
+    then
+      # Install Nodejs
+      echo "Installing NVM"
+      curl -o- https://raw.githubusercontent.com/creationix/nvm/v0.31.4/install.sh | bash
+
+      echo "Installing Nodejs"
+      nvm install node
+      nvm use node
+
+      brew install mongodb
+
+      npm install -g coffee-script
+      npm install -g grunt-cli
+      npm install -g jshint
+      npm install -g less
+
+    fi
+
+    echo -e "${RED}Install Unity3D? ${NC}[y/N]"
+    read -n 1 -r
+    echo    # (optional) move to a new line
+    if [[ $REPLY =~ ^[Yy]$ ]]
+    then
+      brew cask install unity unity-web-player
+
+    fi
+
+    echo -e "${RED}Install Python? ${NC}[y/N]"
+    read -n 1 -r
+    echo    # (optional) move to a new line
+    if [[ $REPLY =~ ^[Yy]$ ]]
+    then
+      # Install Python
+      brew install python
+    fi
+
+    echo -e "${RED}Install Ruby?${NC} [y/N]"
+    read -n 1 -r
+    echo    # (optional) move to a new line
+    if [[ $REPLY =~ ^[Yy]$ ]]
+    then
+      # Install ruby
+      brew install gpg
+      command curl -sSL https://rvm.io/mpapis.asc | gpg --import -
+      \curl -L https://get.rvm.io | bash -s stable
+      source ~/.rvm/scripts/rvm
+      rvm install ruby-2.3.1
+
+      gem install bundler
+      gem install rails
+    fi
+
+    echo -e "${RED}Install Cocoapods?${NC} [y/N]"
+    read -n 1 -r
+    echo    # (optional) move to a new line
+    if [[ $REPLY =~ ^[Yy]$ ]]
+    then
+      # Install Cocoapods
+      sudo gem install cocoapods
+    fi
+
+    echo -e "${RED}Setup for Java Devlopment? ${NC}[y/N]"
+    read -n 1 -r
+    echo    # (optional) move to a new line
+    if [[ $REPLY =~ ^[Yy]$ ]]
+    then
+      brew cask install \
+      java \
+      eclipse-ide \
+      eclipse-java
+    fi
+
+    echo -e "${RED}Setup For Android Developemnt?${NC} [y/N]"
+    read -n 1 -r
+    echo    # (optional) move to a new line
+    if [[ $REPLY =~ ^[Yy]$ ]]
+    then
+      brew cask install \
+      java \
+      eclipse-ide \
+      eclipse-java \
+      android-studio \
+      intellij-idea-ce
+
+      brew install android-sdk
+    fi
+
+    echo -e "${RED}Install Databases? ${NC}[y/N]"
+    read -n 1 -r
+    echo    # (optional) move to a new line
+    if [[ $REPLY =~ ^[Yy]$ ]]
+    then
+      brew install mysql
+      brew install postgresql
+      brew install mongo
+      brew install redis
+      brew install elasticsearch
+
+      # Install mysql workbench
+      # Install Cask
+      brew install caskroom/cask/brew-cask
+      brew cask install --appdir="/Applications" mysqlworkbench
+    fi
+
+
+
     # Install Homebrew Apps
     echo "Installing Homebrew Command Line Tools"
     brew install \
-    mongodb \
     tree \
     wget \
     ack \
     heroku-toolbelt
-    
+
     # Install EMacs
     echo "Installing EMacs"
     brew install emacs --with-cocoa
     brew linkapps emacs
-    
+
     brew tap caskroom/cask
-    
+
     echo "Installing Apps"
     brew cask install \
     google-chrome \
@@ -92,15 +213,19 @@ if type xcode-select >&- && xpath=$( xcode-select --print-path ) &&
     mamp \
     macdown \
     google-drive \
-    iterm2
-    
+    iterm2 \
+    sublime-text \
+    virtualbox \
+    minecraft \
+    scratch
+
     echo "Cleaning Up Cask Files"
     brew cask cleanup
-    
-    echo "Installing Nodejs Packages"
-    sudo npm i -g nodemon gulp simplehttpserver bower
+
+    echo "${GREEN}Thanks for using CleanMacSetup! If you liked it, make sure to go to the Github Repo (https://github.com/adamisntdead/CleanMacSetup) and Star it! If you have any issues, just put them there, and all suggestions and contributions are appreciated!"
+
 else
    echo "Need to install the OSX Command Line Tools (or XCode) First! Starting Install..."
-   # Install XCODE Command Line Tools 
+   # Install XCODE Command Line Tools
    xcode-select --install
 fi
